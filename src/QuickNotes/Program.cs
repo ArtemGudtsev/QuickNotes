c204461b81dc3abfd3ad0d942fee3e50e964e1c2
Gudtsev Artem <Artem.Gudtsev@gmail.com>
2019-12-01 23:08:08 +0100

    move source files from local folder to git repository, very initial version to have a look into idea

---
+++ src/QuickNotes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace QuickNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string folderWithNotes = @"E:\out\qnotes";
+            var currentNotesFile = $"{DateTime.Now:yyyyMMdd}.qnote";
+            var fullPathToNotesFile = Path.Combine(folderWithNotes, currentNotesFile);
+            var isExit = !(args.Length == 1 && args[0] == "-a");
+
+            do
+            {
+                var timePrefix = $"[{DateTime.Now:HH:mm:ss}] ";
+
+                Console.Write(timePrefix);
+
+                var record = Console.ReadLine();
+
+                if (record == "x")
+                {
+                    isExit = true;
+                }
+                else
+                {
+                    record = timePrefix + record;
+                    if (File.Exists(fullPathToNotesFile))
+                    {
+                        File.AppendAllLines(fullPathToNotesFile, new[] { record });
+                    }
+                    else
+                    {
+                        File.WriteAllLines(fullPathToNotesFile, new[] { record });
+                    }
+                }
+            } while (!isExit);
+        }
+    }
+}
